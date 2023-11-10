0daf7bc68dc3ffa294031b3e534def2d61e78d4b
TH <kareme007@gmail.com>
2023-11-10 11:17:08 +0900

    spring으로 person 테이블 작업하기

--- src/main/java/com/green/springfifth/person/model/SelectVo.java
+++ src/main/java/com/green/springfifth/person/model/SelectVo.java
@@ -1,9 +1,10 @@
 package com.green.springfifth.person.model;
 
+import lombok.AllArgsConstructor;
 import lombok.Getter;
-import lombok.Setter;
 
 @Getter
+@AllArgsConstructor
 public class SelectVo {
     private String fname;
     private String lname;
